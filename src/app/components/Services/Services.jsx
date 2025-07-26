0b5aff05480876758d70c113fd68700f304a1081
cnv-nilesh <nilesh@cnvmoney.com>
2025-07-26 15:03:59 +0530

    DOWNLOAD SECTION UPDATED

--- src/app/components/Services/Services.jsx
+++ src/app/components/Services/Services.jsx
@@ -6,50 +6,44 @@ import Image from "next/image";
 import { cart } from "./product";
 
 const Services = () => {
-  const [index, setIndex] = useState(0);
-  const itemWidth = 300; // width in pixels
-  const visibleItems = 4;
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prev) => (prev + 1 >= cart.length ? 0 : prev + 1));
-    }, 4000);
-    return () => clearInterval(interval);
-  }, []);
-
-  const totalWidth = cart.length * itemWidth;
-
   return (
     <div className="px-4 py-8 overflow-hidden bg-slate-100">
       <h2 className="text-2xl font-bold text-center ">
         The Best Service We Offer
       </h2>
 
-      <div className="relative w-full overflow-hidden max-w-[1200px] mx-auto">
-        <motion.div
-          className="flex gap-4"
-          animate={{ x: -index * (itemWidth + 16) }} // 16 = gap
-          transition={{ duration: 0.6, ease: "easeInOut" }}
-          style={{ width: totalWidth }}
-        >
-          {cart.map((item) => (
-            <div
-              key={item.id}
-              className="mt-4 min-w-[300px] max-w-[300px] bg-white shadow-md p-4 rounded-xl flex flex-col items-center text-center mb-6"
-            >
-              <Image
-                src={item.Image}
-                alt={item.Title}
-                width={50}
-                height={50}
-                className="mb-3"
-              />
-              <p className="text-lg font-semibold">{item.Title}</p>
-              <p className="text-sm mt-2 text-gray-600">{item.Desc}</p>
-            </div>
-          ))}
-        </motion.div>
+      <div className="relative w-full overflow-hidden max-w-[1200px] mx-auto flex gap-5">
+        {cart.map((item) => (
+          <div
+            key={item.id}
+            className="mt-4 min-w-[300px] max-w-[300px] bg-white animate-slide shadow-md p-4 rounded-xl flex flex-col items-center text-center mb-6"
+          >
+            <Image
+              src={item.Image}
+              alt={item.Title}
+              width={50}
+              height={50}
+              className="mb-3"
+            />
+            <p className="text-lg font-semibold">{item.Title}</p>
+            <p className="text-sm mt-2 text-gray-600">{item.Desc}</p>
+          </div>
+        ))}
       </div>
+      <style jsx>{`
+        .animate-slide {
+          animation: scroll 2s linear infinite;
+        }
+
+        @keyframes scroll {
+          0% {
+            transform: translateX(0%);
+          }
+          100% {
+            transform: translateX(-50%);
+          }
+        }
+      `}</style>
     </div>
   );
 };
